c9ff00a7f586aa223677ebaaeb97c81826a97a32
Manash Mandal <manashmndl@gmail.com>
2016-05-06 21:51:53 +0600

    TakeOrderUpdated

---
+++ TakeOrderClient/app/src/main/java/com/electroscholars/manash/takeorderclient/MainActivity.java
@@ -0,0 +1,14 @@
+package com.electroscholars.manash.takeorderclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.electroscholars.manash.takeorderclient.HelperClass.Client;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
