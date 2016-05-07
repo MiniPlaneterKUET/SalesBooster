16741c183176868c37b2339804f3c0ec9c1e9855
Manash Mandal <manashmndl@gmail.com>
2016-05-07 12:40:46 +0600

    added chequeDatePicker
    * Fixed Poping up multiple times

--- TakeOrderClient/app/src/main/java/com/electroscholars/manash/takeorderclient/Activities/OrderSubmissionActivity.java
+++ TakeOrderClient/app/src/main/java/com/electroscholars/manash/takeorderclient/Activities/OrderSubmissionActivity.java
@@ -1,8 +1,12 @@
 package com.electroscholars.manash.takeorderclient.Activities;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.DatePicker;
 
@@ -15,48 +19,64 @@ import java.util.Locale;
 
 public class OrderSubmissionActivity extends AppCompatActivity {
 
+    private static final int DATE_DIALOG_ID = 999;
+
     private Calendar calendar;
     private DatePickerDialog chequeDatePickerDialog;
-    private MaterialEditText chequeDate;
+    private MaterialEditText chequeDateEditText;
     private SimpleDateFormat dateFormat;
 
-    private int day, month, year;
+    //It is required to prevent the datepicker from appearing multiple times
+    private boolean isChequeDatePickerOpened = false;
 
-    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener(){
+    //Just in case
+    protected static int day;
+    protected static int month;
+    protected static int year;
+
+
+    class ChequeDateSetListener implements DatePickerDialog.OnDateSetListener {
         @Override
         public void onDateSet(DatePicker datePicker, int year, int month, int day){
-            setDate(day, month, year);
-        }
-    };
+            OrderSubmissionActivity.month = month;
+            OrderSubmissionActivity.year = year;
+            OrderSubmissionActivity.day = day;
+            chequeDateEditText.setText(String.valueOf(day) + " - " + String.valueOf(month + 1) + " - " + String.valueOf(year));
+            isChequeDatePickerOpened = false;
 
-    public void setDate(int d, int m, int y){
-        this.day = d;
-        this.month = m;
-        this.year = y;
+        }
     }
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_submission);
 
         //Initializing
-        chequeDate = (MaterialEditText) findViewById(R.id.chequeDateEditText);
-        dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
-        calendar = Calendar.getInstance();
+        chequeDateEditText = (MaterialEditText) findViewById(R.id.chequeDateEditText);
 
 
-
-        chequeDate.setOnClickListener(new View.OnClickListener() {
+        //If ChequeEditText is clicked then a datepicker would pop up
+        chequeDateEditText.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view){
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(OrderSubmissionActivity
-                        .this, dateSetListener,
-                        year, month, day);
-                datePickerDialog.show();
+                if (!isChequeDatePickerOpened){
+                    isChequeDatePickerOpened = true;
+                    DatePickerDialog dialog = new DatePickerDialog(OrderSubmissionActivity.this, new ChequeDateSetListener(), year, month, day);
+                    dialog.show();
+                }
+                return false;
             }
         });
+
+
+
+
 
     }
 }
