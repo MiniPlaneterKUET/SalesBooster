cbc7ac582b3828e63288d4577b8c7b2f75f5f264
Pial Kanti Samadder <pialkanti2012@gmail.com>
2016-05-06 19:09:15 -0700

    Log in sucessful

--- LogIn_FireBase/app/src/main/java/com/example/pial/login_firebase/Main/LogINActivity.java
+++ LogIn_FireBase/app/src/main/java/com/example/pial/login_firebase/Main/LogINActivity.java
@@ -13,18 +13,27 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.pial.login_firebase.Model.User;
 import com.example.pial.login_firebase.R;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class LogINActivity extends AppCompatActivity {
 
+    private final static String TAG = "Check";
+
     public CoordinatorLayout coordinatorLayout;
-    public com.rengwuxian.materialedittext.MaterialEditText userEditText,passwordEdtText;
+    public com.rengwuxian.materialedittext.MaterialEditText userEditText, passwordEdtText;
     public info.hoang8f.widget.FButton loginButton;
+    public String username;
+    public String password;
+    public Firebase ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +41,10 @@ public class LogINActivity extends AppCompatActivity {
         Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_log_in);
 
-        coordinatorLayout=(CoordinatorLayout)findViewById(R.id.coordinatorLayout);
-        userEditText=(MaterialEditText)findViewById(R.id.usernameEditText);
-        passwordEdtText=(MaterialEditText)findViewById(R.id.passwordEditText);
-        loginButton=(info.hoang8f.widget.FButton)findViewById(R.id.logInButton);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        userEditText = (MaterialEditText) findViewById(R.id.usernameEditText);
+        passwordEdtText = (MaterialEditText) findViewById(R.id.passwordEditText);
+        loginButton = (info.hoang8f.widget.FButton) findViewById(R.id.logInButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,23 +52,63 @@ public class LogINActivity extends AppCompatActivity {
                 //This checks if Internet is turn om or off
 
                 ConnectivityManager cm =
-                        (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
                 NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
                 boolean isConnected = activeNetwork != null &&
                         activeNetwork.isConnectedOrConnecting();
-                if(!isConnected){
+
+                if (isConnected) {
+                    Log.i("Check", isConnected + "");
+                    username = userEditText.getText().toString();
+                    password = passwordEdtText.getText().toString();
+
+                    Log.i(TAG + " username", userEditText.getText().toString());
+                    Log.i(TAG + " password", passwordEdtText.getText().toString());
+
+                    //Checking username and password is not empty
+                    if (!username.isEmpty() && !password.isEmpty()){
+                        Log.i("Check","hahhadhah");
+                        //firebase object
+                        ref = new Firebase("https://sales-booster.firebaseio.com/User").child(username);
+
+                        //getting value from firebase
+
+                        ref.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.exists()) {
+                                    Snackbar.make(coordinatorLayout, "Success", Snackbar.LENGTH_LONG).show();
+
+                                } else {
+                                    Snackbar.make(coordinatorLayout, "Wrong username or password", Snackbar.LENGTH_LONG).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(FirebaseError firebaseError) {
+
+                            }
+                        });
+                    }
+                        else {
+                        //Checking username or password is null
+                        Snackbar.make(coordinatorLayout, "Please enter username or password", Snackbar.LENGTH_LONG).show();
+                    }
+
+
+                } else {
                     //Showing SnackBar that internet is turned off
 
                     Snackbar snackbar = Snackbar
                             .make(coordinatorLayout, "No Inetrnet connection", Snackbar.LENGTH_LONG)
-                            .setAction("Turn on",new View.OnClickListener(){
+                            .setAction("Turn on", new View.OnClickListener() {
                                 public void onClick(View view) {
                                     startActivity(new Intent(Settings.ACTION_WIRELESS_SETTINGS));
                                 }
                             });
 
-                    // Changing message text color
+                    // Changing action message text color
                     snackbar.setActionTextColor(Color.YELLOW);
 
                     snackbar.show();
