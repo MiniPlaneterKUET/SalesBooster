35ebd1e7c6e27b5590907f7b24a3b1cc75606302
Manash Mandal <manashmndl@gmail.com>
2016-05-12 15:35:10 +0600

    added edit text for target and sales

--- SalesVsTargetChart/app/src/main/java/com/electroscholars/manash/salesvstargetchart/SalesVsTargetMainActivity.java
+++ SalesVsTargetChart/app/src/main/java/com/electroscholars/manash/salesvstargetchart/SalesVsTargetMainActivity.java
@@ -16,35 +16,72 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.rengwuxian.materialedittext.MaterialEditText;
+
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import info.hoang8f.widget.FButton;
 
 
 public class SalesVsTargetMainActivity extends AppCompatActivity {
 
+    private static final String REMAINING = "Remaining";
+    private static final String SALES = "Sales";
+
+    private static int SALES_COUNT = 30;
+    private int REMAINING_COUNT = 60;
+    private int TARGET_COUNT = 0;
+
+    private static final String REMAINING_COLOR = "#004D40";
+    private static final String SALES_COLOR = "#8BC34A";
+
+    private MaterialEditText setTargetEditText;
+    private FButton updateTargetButton;
+    private PieChart salesVsTargetPieChart;
+    private PieModel remainingPieModel;
+    private PieModel salesPieModel;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_vs_target_main);
 
-        PieChart pieChart = (PieChart) findViewById(R.id.piechart);
+        salesVsTargetPieChart = (PieChart) findViewById(R.id.salesVsTargetPieChart);
+        setTargetEditText = (MaterialEditText) findViewById(R.id.setTargetEditText);
+        updateTargetButton = (FButton) findViewById(R.id.updateChartButton);
 
-//        pieChart.addPieSlice(new PieModel("Remaining", 70, Color.parseColor("#FE6DA8")));
-//        pieChart.addPieSlice(new PieModel("Sales", 30, Color.parseColor("#FED70E")));
+        salesPieModel = new PieModel(SALES, SalesVsTargetMainActivity.SALES_COUNT, Color.parseColor
+                (SALES_COLOR));
+        remainingPieModel = new PieModel(REMAINING, REMAINING_COUNT, Color.parseColor
+                (REMAINING_COLOR));
 
-        PieModel remainingModel = new PieModel("Remaining", 70, Color.parseColor("#004D40"));
-        remainingModel.setShowLabel(true);
+        //Adding Listener
+        updateTargetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (setTargetEditText.getText().toString().isEmpty()){
+                    Toast.makeText(SalesVsTargetMainActivity.this, "Target is Empty!", Toast
+                            .LENGTH_LONG).show();
+                } else {
+                    TARGET_COUNT = Integer.valueOf(setTargetEditText.getText().toString());
+                    REMAINING_COUNT = TARGET_COUNT - SALES_COUNT;
+                    salesPieModel.setValue(REMAINING_COUNT);
+                    salesVsTargetPieChart.clearChart();
+                    salesVsTargetPieChart.addPieSlice(remainingPieModel);
+                    salesVsTargetPieChart.addPieSlice(salesPieModel);
+                    salesVsTargetPieChart.startAnimation();
+                }
+            }
+        });
 
-        PieModel salesModel = new PieModel("Sales", 30, Color.parseColor("#8BC34A"));
-        salesModel.setShowLabel(true);
+        salesVsTargetPieChart.addPieSlice(salesPieModel);
+        salesVsTargetPieChart.addPieSlice(remainingPieModel);
+        salesVsTargetPieChart.startAnimation();
 
-        pieChart.addPieSlice(remainingModel);
-        pieChart.addPieSlice(salesModel);
-
-        pieChart.startAnimation();
     }
 }
